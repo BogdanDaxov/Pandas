a5960eb2c72561bcf9eb50c6c73b3ddda8c08b6a
BogdanDaxov <145527748+BogdanDaxov@users.noreply.github.com>
2024-11-24 18:17:25 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,11 @@ import pandas as p
 df = p.read_csv("transactions.csv")
 
 g = df.sort_values(by='SUM', ascending=False).loc[df["STATUS"] == "OK",:]
-print("3 самых крупных платежа из реально проведённых:\n"+str(g.loc[5085,'SUM'])+"\n"+str(g.loc[4451,'SUM'])+"\n"+str(g.loc[1348,'SUM'])+"\n")
+g.loc[:,'Unnamed: 0']=[i for i in range(len(g.loc[:,'Unnamed: 0']))]
+b=g[g['Unnamed: 0']==0]
+c=g[g['Unnamed: 0']==1]
+d=g[g['Unnamed: 0']==2]
+print("3 самых крупных платежа из реально проведённых:\n"+str(b.iloc[0,3])+"\n"+str(c.iloc[0,3])+"\n"+str(d.iloc[0,3])+"\n")
 
-a = df.groupby('CONTRACTOR').sum()
+a = df.loc[df["STATUS"] == "OK",:].groupby('CONTRACTOR').sum()
 print("Полная сумма реально проведённых платежей в адрес Umbrella, Inc:"+"\n"+str(a.loc['Umbrella, Inc','SUM']))
